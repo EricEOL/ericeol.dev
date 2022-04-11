db6f33ead321704bb7d67303017a5692996233a5
EricEOL <lopes.eric051@gmail.com>
2022-04-10 22:03:00 -0300

    Botao para mudança de tema

--- src/app/core/components/header/navbar/navbar.component.ts
+++ src/app/core/components/header/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
-  theme(): boolean {
-    document.body.classList.toggle('lightTheme');
-    return true;
+  lightTheme: boolean = false;
+
+  ngOnInit(): void {
+    const button = document.querySelector('#circle');
+    button?.classList.add('unchecked');
   }
 
+  theme(): void {
+    document.body.classList.toggle('lightTheme');
+
+    if(this.lightTheme == false) this.lightTheme = true;
+    else this.lightTheme = false;
+
+    const button = document.querySelector('#circle');
+
+    if(this.lightTheme == false) {
+      button?.classList.remove('unchecked');
+      button?.classList.add('checked');
+    } else {
+      button?.classList.remove('checked');
+      button?.classList.add('unchecked');
+    } 
+
+  }
 }
