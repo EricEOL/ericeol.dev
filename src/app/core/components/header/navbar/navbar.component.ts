ad4a82f0f52fe660f7decf36b5ef47d3e4661454
EricEOL <lopes.eric051@gmail.com>
2022-04-25 22:25:18 -0300

    Menu mobile

--- src/app/core/components/header/navbar/navbar.component.ts
+++ src/app/core/components/header/navbar/navbar.component.ts
@@ -12,23 +12,37 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
     const button = document.querySelector('#circle');
     button?.classList.add('unchecked');
+
+    this.activeMobileMenu();
   }
 
   theme(): void {
     document.body.classList.toggle('lightTheme');
 
-    if(this.lightTheme == false) this.lightTheme = true;
+    if (this.lightTheme == false) this.lightTheme = true;
     else this.lightTheme = false;
 
     const button = document.querySelector('#circle');
 
-    if(!this.lightTheme) {
+    if (!this.lightTheme) {
       button?.classList.remove('checked');
       button?.classList.add('unchecked');
     } else {
       button?.classList.remove('unchecked');
       button?.classList.add('checked');
-    } 
+    }
 
+  }
+
+  activeMobileMenu(): void {
+    const menuMobileBtn = document.querySelector(".menu-hamburger");
+    const menuMobileLinks = document.querySelector(".nav-mobile");
+
+    if(menuMobileBtn && menuMobileLinks) {
+      menuMobileBtn.addEventListener('click', () => {
+        menuMobileBtn.classList.toggle('is-active');
+        menuMobileLinks.classList.toggle('is-active');
+      });
+    }
   }
 }
