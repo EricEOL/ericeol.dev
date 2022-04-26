6e14532ffd37d66555840ac9d6343baddc3be230
EricEOL <lopes.eric051@gmail.com>
2022-04-26 12:19:30 -0300

    Ajustanto página home para mobile

--- src/app/shared/socialmedia/socialmedia.component.ts
+++ src/app/shared/socialmedia/socialmedia.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-social-media',
   templateUrl: './socialmedia.component.html',
-  styleUrls: ['./socialmedia.component.css']
+  styleUrls: ['./socialmedia.component.css', 'socialmedia-mobile.component.css']
 })
 export class SocialMediaComponent {}
